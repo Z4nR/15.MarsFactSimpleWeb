ee2302f94bb49b4829525db0ef57f07429752e61
Zulham Ari <ari.comunity1@gmail.com>
2021-08-31 22:53:27 +0700

    add to top button

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
-window.onclick = function getData() {
-    var click = document.getElementById("list-drop");
-    if (click.style.display === "none") {
-        click.style.display = "block";
+const listDrop = document.getElementById("list-drop");
+listDrop.style.display = "none";
+
+function getData() {
+    if (listDrop.style.display === "none") {
+        listDrop.style.display = "block";
     } else {
-        click.style.display = "none";
+        listDrop.style.display = "none";
+    }
+}
+
+const topBtn = document.getElementById("top-img")
+
+function goToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+window.onscroll = function () { scrollDown() };
+
+function scrollDown() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
     }
 }
